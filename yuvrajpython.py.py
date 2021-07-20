4473586e30e5d7143a37564faa574ed0b1671e59
YOUSEE1897 <87479789+YOUSEE1897@users.noreply.github.com>
2021-07-20 14:26:13 +0530

    Add files via upload

---
+++ yuvrajpython.py.py
@@ -0,0 +1,16 @@
+
+
+print("TAERG gniod era uoY"[ : : -1])
+print("This is a simple maths power syntax")
+print(3 ** 3)
+
+print("Some more changes to this file...")
+#Some more changes
+
+
+
+import time
+time.sleep(5)
+
+
+
